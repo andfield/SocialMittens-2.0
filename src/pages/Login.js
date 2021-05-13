56e5b1638fa729402605ec41c5520f079c96d63c
andfield <siddharththakur76@gmail.com>
2021-05-13 19:16:43 +1200

    Login styles done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,14 +24,52 @@ function Login() {
 
   const login = () => {};
 
-  return <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
-          <img src="/images/iphone-with-profile.jpg" alt="iphone with profile" className="max-w-full"/>
+  return (
+    <div className="container grid sm:flex justify-items-center items-center sm:mx-auto max-w-screen-md h-screen sm:flex-row">
+     
+      <div className="w-3/5 sm:w-1/2 hidden sm:flex">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iphone with profile"
+          className="max-w-full"
+        />
       </div>
-      <div className="flex flex-col w-2/5">
-          <p>Form space</p>
+      
+      <div className="flex flex-col w-10/12 sm:w-2/5 items-center bg-white border border-gray-primary mb-4">
+        <h1 className="flex justify-center ml-2 mt-2 w-full font-sans text-2xl font-bold">
+          Social Mittens
+        </h1>
+        {error && <p className="text-red-primary mb-4 text-xs">{error}</p>}
+        <form onSubmit={login} method="POST">
+          <input
+            aria-label="Enter you Email"
+            type="text"
+            placeholder="Email address"
+            className="text-grey-base border-gray-primary mb-2 mr-3 mt-4 px-4 py-5 w-full h-2 text-sm border rounded"
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            aria-label="Enter you password"
+            type="password"
+            placeholder="Password"
+            className="text-grey-base border-gray-primary mb-2 mr-3 px-4 py-5 w-full h-2 text-sm border rounded"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button
+            disabled={isInvalid}
+            type="submit"
+            className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && "opacity-50"}`}
+          >
+            login
+          </button>
+        </form>
+        <div className="mt-5 border-gray-primary flex flex-col items-center justify-center p-4 w-full bg-white border">
+        <p className="text-sm">Don't have an account?{` `}</p>
       </div>
-  </div>;
+      </div>
+    </div>
+  );
 }
 
 export default Login;
